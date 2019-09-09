fbde8e86a3af22d594d51e5bdbfda16316a2ea0b
jneilliii <jneilliii+github@gmail.com>
2019-09-08 22:27:33 -0400

    Change event to connected and force filename for loading.

--- octoprint_autoselect/__init__.py
+++ octoprint_autoselect/__init__.py
@@ -7,46 +7,22 @@ import octoprint.filemanager
 from octoprint.filemanager.destinations import FileDestinations
 
 class AutoselectPlugin(octoprint.plugin.EventHandlerPlugin):
-	def __init__(self):
-		self._event = None
-		if hasattr(octoprint.events.Events, "FILE_ADDED"):
-			# OctoPrint >= 1.3.3: we can monitor the FileAdded event as triggered
-			# by the file manager
-			self._event = octoprint.events.Events.FILE_ADDED
-			def to_storage_and_name(payload):
-				return payload["storage"], payload["path"]
-		else:
-			# OctoPrint < 1.3.3: we can only react to the upload event, files added
-			# through e.g. the watched folder or other means than the REST API are
-			# left out. See also #1
-			self._event = octoprint.events.Events.UPLOAD
-			def to_storage_and_name(payload):
-				return payload["target"], payload["path"]
-
-		self._to_storage_and_name = to_storage_and_name
-
 	def on_event(self, event, payload):
-		if event != self._event:
+		if event != octoprint.events.Events.CONNECTED:
 			return
 
 		if not self._printer.is_ready():
 			self._logger.debug("Printer is not ready, not autoselecting uploaded file")
 			return
 
-		storage, filename = self._to_storage_and_name(payload)
+		filename = "autoprint.gcode" # put your filename here.
 		if not octoprint.filemanager.valid_file_type(filename, type="machinecode"):
 			self._logger.debug("File is not a machinecode file, not autoselecting")
 			return
 
-		if storage == FileDestinations.SDCARD:
-			path = filename
-			sd = True
-		else:
-			path = self._file_manager.path_on_disk(storage, filename)
-			sd = False
-
-		self._logger.info("Selecting {} on {} that was just uploaded".format(filename, storage))
-		self._printer.select_file(path, sd, False)
+		self._logger.info("Selecting {} since we just connected.".format(filename))
+		path = self._file_manager.path_on_disk("local", filename)
+		self._printer.select_file(path, False, False)
 
 	##~~ Softwareupdate hook
 
